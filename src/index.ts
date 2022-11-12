e6cd77de12b8799775551e0bdc6c8cfd7ade7a05
Kieran Mann <kieran.mann@o8t.com>
2022-11-11 16:37:54 -0800

    support worker/fork execution modes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-import { run } from './runner.js';
+import { ExecutionMode, run } from './runner.js';
+import { parseArgs } from 'node:util';
 
-run({ testPaths: ['./dist/example.test.js'] }).then((exitCode) => {
+const { values } = parseArgs({
+    options: {
+        executionMode: {
+            type: 'string',
+        },
+    },
+});
+
+function validateExecutionMode(
+    maybeExecutionMode?: string
+): asserts maybeExecutionMode is ExecutionMode {
+    if (!maybeExecutionMode) {
+        throw new Error(
+            'Unspecifed execution mode. Please pass `--executionMode` with a value of `worker` or `fork`'
+        );
+    }
+
+    if (!['worker', 'fork'].includes(maybeExecutionMode)) {
+        throw new Error(`Unhandled execution mode option ${maybeExecutionMode}`);
+    }
+}
+
+validateExecutionMode(values.executionMode);
+
+run({
+    testPaths: ['./dist/example.test.js'],
+    executionMode: values.executionMode,
+}).then((exitCode) => {
     process.exit(exitCode);
 });
--- src/runner.ts
+++ src/runner.ts
@@ -1,31 +1,43 @@
 // import { Worker } from 'node:worker_threads';
 import { fork } from 'node:child_process';
+import { Worker } from 'node:worker_threads';
 import path from 'node:path';
+
+export type ExecutionMode = 'worker' | 'fork';
 
 interface RunOptions {
     testPaths: string[];
+    executionMode: ExecutionMode;
+}
+
+function getExecutor(testPath: string, executionMode: ExecutionMode) {
+    if (executionMode === 'worker') {
+        return new Worker(testPath);
+    }
+    return fork(path.resolve(testPath), {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+    });
 }
 
 export async function run(options: RunOptions): Promise<number> {
     const testRuns: Promise<number>[] = [];
 
     for (const testPath of options.testPaths) {
-        const testWorker = fork(path.resolve(testPath), {
-            stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
-        });
+        const executor = getExecutor(
+            path.resolve(testPath),
+            options.executionMode
+        );
 
-        console.log('Forked worker');
-
-        testWorker.on('error', (errMsg) => {
+        executor.on('error', (errMsg) => {
             console.error(errMsg);
         });
 
-        testWorker.on('message', (msg) => {
+        executor.on('message', (msg) => {
             console.log('RECEIVING SIDE', msg);
         });
 
         const testExitPromise = new Promise<number>((resolve) =>
-            testWorker.on('exit', () => {
+            executor.on('exit', () => {
                 resolve(0);
             })
         );
